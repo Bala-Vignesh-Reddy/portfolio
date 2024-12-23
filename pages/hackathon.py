d34d82293b1f2281da3098d150581b05f7c11ca0
Bala-Vignesh-Reddy <reddybalavignesh9979@gmail.com>
2024-12-23 23:01:39 +0530

    first version

--- pages/hackathon.py
+++ pages/hackathon.py
@@ -37,7 +37,7 @@ with t2:
     """,
       'Skills', 
       '`TensorFlow`, `YOLO` , `Object Detection`, `IOT`, `Instance Detection`',
-      'https://github.com/Bala-Vignesh-Reddy/HackNUthon-Unkils',
+      'https://github.com/Bala-Vignesh-Reddy/Hack4in-Unkils',
       'Smart Imaging Device',
       'Hack For India',
     img2)
